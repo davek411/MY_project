f64577d36f16d81b1138fb5f7961f6f0c897b1a0
davek411 <141879647+davek411@users.noreply.github.com>
2024-03-14 16:46:47 +0530

    Update InputVerticle.java

--- InputVerticle.java
+++ InputVerticle.java
@@ -29,56 +29,19 @@ public class InputVerticle extends AbstractVerticle {
 
       logger.info("inside start method");
 
-      // vertx.setPeriodic(1000,this::updateTemperature);
-
-//    pgPool = PgPool.pool(vertx, new PgConnectOptions()
-//      .setHost("127.0.0.1")
-//      .setUser("postgres")
-//      .setDatabase("postgres")
-//      .setPassword("vertx"), new PoolOptions());
-
-
+    
       //ROUTING TO THE METHODS AND HANDLER FUNCTION
       Router router = Router.router(vertx);
       router.get("/calculate").handler(this::handleCalculateRequest);
-      System.out.println("chalo chaliye");
-
-
-//    router.get("/add").handler(this::addData);
-//    router.get("/subtract").handler(this::subtractData);
-//    router.get("/multiply").handler(this::multiplyData);
-//    router.get("/divide").handler(this::divideData);
-////
-      // vertx.eventBus().consumer("Data added".this::datadded);
-
-
 
       vertx.createHttpServer().requestHandler(router)
         .listen(8080)
         .onSuccess(ok -> {
-//logger.info("http server running :http://0.0.0.0:{}"+httpPort);
           startPromise.complete();
         })
         .onFailure(startPromise::fail);
-//      req.response()
-//        .putHeader("content-type", "text/plain")
-//        .end("Hello from Vert.x!");
-//    }).listen(8888, http -> {
-//      if (http.succeeded()) {
-      //startPromise.complete();
-//        System.out.println("HTTP server started on port 8888");
-//      } else {
-//        startPromise.fail(http.cause());
-//      }
-//    });
-
-
+//    
     }
-
-//  private void getalldata(RoutingContext routingContext)
-//  {
-//    logger.info("Requesting all daat from {}",routingContext.request.remoteAddress());
-//  }
 
     private void handleCalculateRequest(RoutingContext routingContext) {
       String xValue = routingContext.request().getParam("x");
@@ -98,97 +61,7 @@ public class InputVerticle extends AbstractVerticle {
         }
       });
 
-//    private void addData (RoutingContext context){
-//      //logger.info("Processing HTTP request from " + context.request().absoluteURI());
-//      String x_value = context.request().getParam("x");
-//      String y_value = context.request().getParam("y");
-//
-//      int x = Integer.parseInt(x_value);
-//      int y = Integer.parseInt(y_value);
-//
-//      int result = x + y;
-////    pgPool.query("INSERT INTO computation_history (x, y, operation, result) VALUES ($1, $2, $3, $4)")
-////      .execute(Tuple.of(x, y, "add", result))
-////      .onSuccess(rows -> {
-////        // Send response with computation result
-////
-////      })
-////      .onFailure(err -> {
-////        // Handle database insertion error
-////        logger.error("Failed to insert computation result into the database: " + err.getMessage());
-////        context.response().setStatusCode(500).end("Internal Server Error");
-////      });
-////  }
-//      context.response()
-//        .putHeader("Content-Type", "text/plain")
-//        .end("Result: " + result);
-//
-//
-//    }
-//    private void subtractData (RoutingContext context){
-//      //logger.info("Processing HTTP request from " + context.request().absoluteURI());
-//      String x_value = context.request().getParam("x");
-//      String y_value = context.request().getParam("y");
-//
-//      int x = Integer.parseInt(x_value);
-//      int y = Integer.parseInt(y_value);
-//
-//      int result = x - y;
-//
-//
-//      context.response()
-//        .putHeader("Content-type", "text/plain")
-//        .end("Result: " + result);
-//    }
-//    private void divideData (RoutingContext context){
-//      //logger.info("Processing HTTP request from " + context.request().absoluteURI());
-//      String x_value = context.request().getParam("x");
-//      String y_value = context.request().getParam("y");
-//
-//      int x = Integer.parseInt(x_value);
-//      int y = Integer.parseInt(y_value);
-//
-//      int result = x / y;
-//
-//
-//      context.response()
-//        .putHeader("Content-type", "text/plain")
-//        .end("Result: " + result);
-//    }
-//    private void multiplyData (RoutingContext context){
-//      //logger.info("Processing HTTP request from " + context.request().absoluteURI());
-//      String x_value = context.request().getParam("x");
-//      String y_value = context.request().getParam("y");
-//
-//      int x = Integer.parseInt(x_value);
-//      int y = Integer.parseInt(y_value);
-//
-//      int result = x * y;
-//
-//
-//      context.response()
-//        .putHeader("Content-type", "text/plain")
-//        .end("Result: " + result);
-//    }
 
-      // void putHeader(String s, String contentType) {
-
-//  private JsonObject createPayload() {
-//    return new JsonObject()
-//      .put("uuid",uuid)
-//      .put("temperatur",temperature)
-//      .put("timestamp",System.currentTimeMillis());
-//  }
-
-//  private void updateTemperature(Long id) {
-//    temperature = temperature + (random.nextGaussian()/2.0d);
-//    logger.info("Temperature updated: {}" + temperature);
-//
-//    vertx.eventBus()
-//      .publish("temperature.updates", createPayload());
-//
-//
-//  }
     }
   }
 
